af24f014523caef98d190a74a97b81bdf888901c
Peter Bentley <bentleyfineart@gmail.com>
2014-12-23 20:54:01 -0800

    Still trying to fix this

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,5 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:edit, :update, :destroy]
-  before_action :authenticate_user, except: [:index, :show]
 
   def index
     @pins = Pin.all
@@ -11,14 +9,14 @@ class PinsController < ApplicationController
   end
 
   def new
-    @pin = current_user.pins.build
+    @pin = Pin.new
   end
 
   def edit
   end
 
   def create
-    @pin = current_user.pins.build(pin_params)
+    @pin = Pin.new(pin_params)
     if @pin.save
       redirect_to @pin, notice: 'Pin was successfully created.'
     else
@@ -43,11 +41,6 @@ class PinsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_pin
       @pin = Pin.find(params[:id])
-    end
-
-    def correct_user
-      @pin = current_user.pins.find_by (id: params[:id])
-      redirect_to pins_path, notice: "You're not allowed to view this page" if @pin.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
