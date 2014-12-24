227099bcb5b3b54d7a99af523f9637445956369a
Peter Bentley <bentleyfineart@gmail.com>
2014-12-23 22:37:52 -0800

    This version doesn't work, but I'm committing it so I can send along to the One Month folks.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :pins
 
-  devise_for :users
   root "pages#home"
   get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
